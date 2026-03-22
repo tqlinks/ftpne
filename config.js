1c04d286f85532728813ca02682f329e4310ebd4
studio <36976792+tqlinks@users.noreply.github.com>
2026-03-22 19:53:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG = {
     APP_NAME: "Game Team Manager",
-    API_URL: "https://script.google.com/macros/s/AKfycbzN2tBFgiKb3mMA1WhN2VV_lJUKMIf-UsVK7TMwNhxbqP6IyYEkCO3G9tITvFJp1mmJ2A/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzN2tBFgiKb3mMA1WhN2VV_lJUKMIf-UsVK7TMwNhxbqP6IyYEkCO3G9tITvFJp1mmJ2A/exec",
     REFRESH_RATE: 10000 // 1 phút cập nhật 1 lần
 };
