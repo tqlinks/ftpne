d54b537a4681a28615b4d4d61e4347dbc9c4298c
studio <36976792+tqlinks@users.noreply.github.com>
2026-03-22 19:56:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-const APP_CONFIG = {
-    APP_NAME: "Game Team Manager",
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzN2tBFgiKb3mMA1WhN2VV_lJUKMIf-UsVK7TMwNhxbqP6IyYEkCO3G9tITvFJp1mmJ2A/exec",
-    REFRESH_RATE: 10000 // 1 phút cập nhật 1 lần
+const CONFIG = {
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxAwgFgId1sozjEphwxHJqWmFmYW2xlZqsygoKHcTnYp4tEJdxYmwPsZxSvkBr-VN_5Nw/exec"
 };
