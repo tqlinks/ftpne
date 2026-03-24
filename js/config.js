b5afb147471f9a2cbf767a5d370fededb09a0842
studio <36976792+tqlinks@users.noreply.github.com>
2026-03-25 04:34:24 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxHUpTYEApI0oOiyZg-kBTtYzyN40Tni2wyg0bzC23ZPx5bMv5E-ZeBWgXe1GFwgINajA/exec"
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw-R7VZ7-P4n7VkXdi6ZpmIBLDRyylE_gsIqaRiiME2HonTIxGnyuB5i3nEaA6ohV2eFg/exec"
 };
